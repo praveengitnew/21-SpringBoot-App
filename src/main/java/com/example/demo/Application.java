a77156f529c2633e9a3d652100402aaa19f7902c
praveenkumar <praveenkumarasolla@gmail.com>
2021-04-15 03:52:44 +0530

    variables added in class

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		//changes for his-134
+               int a=10;
 		run.close(){
 			
 		}
