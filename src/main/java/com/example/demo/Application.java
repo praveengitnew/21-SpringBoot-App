810d98d9c2c8fa67d176a1228c28e53bd304e5a9
praveenkumar <praveenkumarasolla@gmail.com>
2021-04-15 05:17:30 +0530

    HIS-200:CHANGES

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -9,6 +9,10 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		//changes for his-134
+         //HIS-200 related changes
+          public void doProcess(){
+
+            }
 		run.close(){
 			
 		}
